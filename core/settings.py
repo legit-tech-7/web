6711db25ce9576c321cc48bb7fbe7e8f07ba2337
joshua <joshuachukwuemeka52@gmail.com>
2025-07-25 16:54:17 +0100

    fix: added render domain to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z5ay6s2@j)_fdd&z8@p^hova%0!22-uwoac9ejac6*3!(vl!s5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
